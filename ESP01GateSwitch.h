6fcad104b6664a855c2ae76cb6e4ae57816ed80d
Necroneco <caibinqing@live.com>
2024-02-29 23:28:55 +0800

    esp01

--- ESP01GateSwitch.h
+++ ESP01GateSwitch.h
@@ -7,13 +7,10 @@ class ESP01GateSwitch : public Component, public Switch {
 public:
   int phone = 0;
   int lock = 2;
-  // int led = 2;
 
   void setup() override {
     pinMode(phone, OUTPUT);
     pinMode(lock, OUTPUT);
-    // pinMode(led, OUTPUT);
-    // digitalWrite(led, LED_OFF);
   }
 
   void write_state(bool state) override {
@@ -23,16 +20,12 @@ public:
 
       // ESP_LOGD("gate_switch", "write_state START!");
 
-      // digitalWrite(led, LED_ON);
       digitalWrite(phone, HIGH);
       delay(600);
-      // digitalWrite(led, LED_OFF);
       digitalWrite(lock, HIGH);
       delay(250);
-      // digitalWrite(led, LED_ON);
       digitalWrite(lock, LOW);
       delay(1250);
-      // digitalWrite(led, LED_OFF);
       digitalWrite(phone, LOW);
 
       // ESP_LOGD("gate_switch", "write_state DONE!");
