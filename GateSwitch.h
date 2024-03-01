83b0d4e1f241d763f2fe7b02a99db8d6fc4a29be
Necroneco <caibinqing@live.com>
2024-03-01 22:04:54 +0800

    gate01

--- GateSwitch.h
+++ GateSwitch.h
@@ -3,17 +3,15 @@
 #define LED_ON HIGH
 #define LED_OFF LOW
 
+// ESP01
 class GateSwitch : public Component, public Switch {
 public:
-  int phone = 25;
-  int lock = 26;
-  int led = 2;
+  int phone = 0; // GPIO0
+  int lock = 2; // GPIO2
 
   void setup() override {
     pinMode(phone, OUTPUT);
     pinMode(lock, OUTPUT);
-    pinMode(led, OUTPUT);
-    digitalWrite(led, LED_OFF);
   }
 
   void write_state(bool state) override {
@@ -23,16 +21,12 @@ public:
 
       // ESP_LOGD("gate_switch", "write_state START!");
 
-      digitalWrite(led, LED_ON);
       digitalWrite(phone, HIGH);
       delay(600);
-      digitalWrite(led, LED_OFF);
       digitalWrite(lock, HIGH);
       delay(250);
-      digitalWrite(led, LED_ON);
       digitalWrite(lock, LOW);
       delay(1250);
-      digitalWrite(led, LED_OFF);
       digitalWrite(phone, LOW);
 
       // ESP_LOGD("gate_switch", "write_state DONE!");
